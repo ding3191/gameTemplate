ec4be4d8bda078f05a6847a61a9fad5fdaf65cd1
ding3191 <200042@hkis.edu.hk>
2015-12-13 14:31:05 +0800

    changed functions

--- gamedev.js
+++ gamedev.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
 	$("#start").click(function() {
 		$("#exploreBody").css("background-image", "url('img/2doors.jpg')");
 		$("#goRight1").show();
-		$("#q1").hide();
+		$("#q1").show();
 		$("#q2").hide();
 		$("#q3").hide();
 		$("#goLeft1").show();
@@ -15,8 +15,8 @@ $( document ).ready(function() {
 		$("#q1").hide();
 		$("#q2").show();
 		$("#q3").hide();
-		$("#goRight-Right1").show();
-		$("#goRight-Left1").show();
+		$("#goRight1-Right1").show();
+		$("#goRight1-Left1").show();
 	});
 
 	$("#goLeft1").click(function() {
@@ -25,59 +25,43 @@ $( document ).ready(function() {
 		$("#q1").hide();
 		$("#q2").hide();
 		$("#q3").hide();
-		$("#goRight-Right1").hide);
-		$("#goRight-Left1").hide();
+		$("#goRight1-Right1").hide);
+		$("#goRight1-Left1").hide();
 	});
 	
-	$("#goLeft1").click(function() {
+	$("#goRight1-Right2").click(function() {
+		$("#exploreBody").css("background-image", "url('img/2doors.jpg')");
+		$("#goRight1").hide();
+		$("#q1").hide();
+		$("#q2").hide();
+		$("#q3").show();
+		$("#goRight1-Right2").show();
+		$("#goRight1-Left2").show();
+	});
+	
+	$("#goRight1-Left2").click(function() {
 		$("#exploreBody").css("background-image", "url('img/rko.png')");
 		$("#goRight1").hide();
 		$("#q1").hide();
 		$("#q2").hide();
 		$("#q3").hide();
-		$("#goRight-Right1").hide);
-		$("#goRight-Left1").hide();
+		$("#goRight1-Right2").hide);
+		$("#goRight1-Left2").hide();
 	});
-	
-	$("#goRight-Straight3").click(function() {
+
+	$("#goRight2-Right3").click(function() {
 		$("#exploreBody").css("background-image", "url('img/2doors.jpg')");
-		$("#goRight-Straight3").hide();
-		$("#goRight-Right2").show();
+		$("#goRight1").hide();
+		$("#q1").hide();
+		$("#q2").hide();
+		$("#q3").hide();
 	});
 
-	$("#goRight-Right2").click(function() {
-		$("#exploreBody").css("background-image", "url('img/2doors.jpg')");
-		$("#goRight-Right2").hide();
-		$("#goBack").show();
+	$("#goRight2-Left3").click(function() {
+		$("#exploreBody").css("background-image", "url('img/rko.png')");
+		$("#goRight1").hide();
+		$("#q1").hide();
+		$("#q2").hide();
+		$("#q3").hide();
 	});
-
-
-	$("#goLeft1").click(function() {
-		$("#exploreBody").css("background-image", "url('img/2doors.jpg')");
-		$("#goLeft1").hide();
-		$("#goLeft2").show();
-	});
-
-	$("#goLeft-Straight1").click(function() {
-		$("#exploreBody").css("background-image", "url('img/2doors.jpg')");
-		$("#goLeft2").hide();
-		$("#goLeft3").show();
-	});
-
-	$("#goLeft-Straight2").click(function() {
-		$("#exploreBody").css("background-image", "url('img/2doors.jpg')");
-		$("#goLeft3").hide();
-		$("#goBack").show();
-	});
-
-
-	$("#goBack").click(function() {
-		$("#exploreBody").css("background-image", "url('img/2doors.jpg')");
-		$("#goBack").hide();
-		$("#goRight1").show();
-		$("#goLeft1").show();
-	});
-
-
-
 });
