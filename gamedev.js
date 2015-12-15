6dcd15448cab077477f9ef2210ba6f5cca37700f
ding3191 <200042@hkis.edu.hk>
2015-12-15 11:01:50 +0800

    changed buttons

--- gamedev.js
+++ gamedev.js
@@ -8,15 +8,16 @@ $( document ).ready(function() {
 		$("#q3").hide();
 		$("#goLeft1").show();
 
-
 	$("#goRight1").click(function() {
 		$("#exploreBody").css("background-image", "url('img/2doors.jpg')");
 		$("#goRight1").hide();
 		$("#q1").hide();
 		$("#q2").show();
 		$("#q3").hide();
-		$("#goRight1-Right1").show();
-		$("#goRight1-Left1").show();
+		$("#goRight1-Right2").show();
+		$("#goRight1-Left2").show();
+		$("#goRight2-Right3").hide();
+		$("#goRight2-Left3").hide();
 	});
 
 	$("#goLeft1").click(function() {
